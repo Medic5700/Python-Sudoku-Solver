df12e5bad2d22e910ff4809c1c9afa9820cb46fd
Medic5700 <Medic5700@users.noreply.github.com>
2022-05-06 11:34:39 -0400

    Minor

--- Sudoku.py
+++ Sudoku.py
@@ -2,6 +2,8 @@
 A small program to solve a sudoku puzzle
 
 By Medic5700
+
+#TODO be able to detect if a sudoku puzzle has multiple solutions
 '''
 
 from typing import List
@@ -104,7 +106,7 @@ def field9x9_toString(field : List[List[int or None]], possible : List[List[List
                output += "------+------+------\n"
      return output
 
-def pencil9x9_singleCell(field : List[List[int or None]], y : int, x : int) -> List[bool]:
+def pencil9x9_singleCell(field : list[list[int | None]], y : int, x : int) -> list[bool]:
      '''Takes in a sudoku board, and returns a list of lists representing the pencil marks for the cell at (i,j).
      '''
      assert type(field) is list
@@ -228,7 +230,7 @@ def solveSudoku(field : List[List[int or None]]) -> List[List[int or None]] or N
           if possible[minI][minJ][i]:
                tempField[minI][minJ] = i + 1
                
-               attemptField = solveSudoku(tempField)
+               attemptField = solveSudoku9x9(tempField)
                if attemptField != None:
                     return attemptField
      
@@ -271,45 +273,11 @@ if __name__ == "__main__":
 
      testField : str
      
-     testField = """-7-9-----
-                    --3-712--
-                    --485--73
-                    --1---5-7
-                    -465-7381
-                    5-71--9-2
-                    7---198--
-                    --82657--
-                    --97-8-2-"""
-     # solved
-     # 275936418
-     # 683471295
-     # 194852673
-     # 821693547
-     # 946527381
-     # 537184962
-     # 762319854
-     # 418265739
-     # 359748126
+     testField = """-7-9----- --3-712-- --485--73 --1---5-7 -465-7381 5-71--9-2 7---198-- --82657-- --97-8-2-""" # formatted as a single line for easy copy-pasting into terminal
+     # solved =    """275936418 683471295 194852673 821693547 946527381 537184962 762319854 418265739 359748126"""
  
-     testField = """---74---6
-                    4-68--5-7
-                    7---9---4
-                    -3-9847--
-                    82-6134-9
-                    -4----3--
-                    -6237---5
-                    --54-9---
-                    -7--612-8"""
-     # solved
-     # 218745936
-     # 496832517
-     # 753196824
-     # 531974762
-     # 827613459
-     # 649257381
-     # 962378145
-     # 185429673
-     # 374561298
+     testField = """---74---6 4-68--5-7 7---9---4 -3-9847-- 82-6134-9 -4----3-- -6237---5 --54-9--- -7--612-8""" # formatted as a single line for easy copy-pasting into terminal
+     # solved =    """218745936 496832517 753196824 531974762 827613459 649257381 962378145 185429673 374561298"""
      
      field = stringToField(testField)
 
@@ -321,7 +289,7 @@ if __name__ == "__main__":
 
      print(field9x9_toString(field, possible))
 
-     print(field9x9_toString(solveSudoku(field)))
+     print(field9x9_toString(solveSudoku9x9(field)))
 
-     print(verify9x9(solveSudoku(field)))
+     print(verify9x9(solveSudoku9x9(field)))
 
